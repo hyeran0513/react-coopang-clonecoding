0757a5998eff9e4e367efce5ccaf37e77f837c09
hyeran0513 <hr_0513@naver.com>
2024-10-02 17:41:20 +0900

    footer UI 생성

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import ToastProvider from "@/components/toastProvider/ToastProvider";
+import Footer from '@/layouts/footer/Footer';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ToastProvider />
         {children}
+        <Footer />
       </body>
     </html>
   );
