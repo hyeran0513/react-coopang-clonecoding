f8e5af0d148807b45c4845b923dd15e431388282
hyeran0513 <hr_0513@naver.com>
2024-10-02 17:17:29 +0900

    firebase 비밀번호 초기화 기능 추가

--- src/app/(auth)/reset/ResetClient.jsx
+++ src/app/(auth)/reset/ResetClient.jsx
@@ -1,11 +1,14 @@
 'use client'
 import React, { useState } from 'react'
-import styles from './Reset.module.scss'
-import Loader from '@/components/loader/Loader'
-import Input from '@/components/input/Input'
-import Heading from '@/components/heading/Heading'
-import Button from '@/components/button/Button'
-import Link from 'next/link'
+import styles from './Reset.module.scss';
+import Loader from '@/components/loader/Loader';
+import Heading from '@/components/heading/Heading';
+import Input from '@/components/Input/Input';
+import Button from '@/components/button/Button';
+import Link from 'next/link';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '@/firebase/firebase';
+import { toast } from 'react-toastify';
 
 const ResetClient = () => {
 
@@ -13,8 +16,20 @@ const ResetClient = () => {
   const [isLoading, setisLoading] = useState(false);
 
   const resetPassword = (e) => {
-    e.preventDefault;
-    setIsLoading(true);
+    e.preventDefault();
+    setisLoading(true);
+
+    sendPasswordResetEmail(auth, email)
+        .then(() => {
+          setisLoading(false);
+
+          toast.success("비밀번호 업데이트를 위해서 이메일을 체크해주세요.");
+        })
+        .catch((error) => {
+          setisLoading(false);
+
+          toast.error(error.message);
+        })
   }
 
   return (
